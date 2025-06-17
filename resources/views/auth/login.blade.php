80161196df0f5d6a48707261c0ba10dc2ef2dba6
Mace Ndhlovu <mercysndlovu@gmail.com>
2025-06-17 21:55:11 +0200

    modifying login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,11 @@
 <x-guest-layout>
-    <div class="min-h-screen flex">
-        <!-- Left Side Image -->
+    <div class="flex min-h-screen">
+        <!-- Left Side: Image -->
         <div class="w-1/2 hidden lg:block">
-            <img src="{{ asset('login_promo.jpg') }}" alt="Login Image"
-                 class="object-cover w-full h-full" />
+            <img src="{{ asset('images/login_promo.jpg') }}" alt="Login Promo" class="w-full h-full object-cover">
         </div>
 
-        <!-- Right Side Form -->
+        <!-- Right Side: Login Form -->
         <div class="w-full lg:w-1/2 flex items-center justify-center p-8">
             <div class="w-full max-w-md">
                 <!-- Session Status -->
@@ -17,39 +16,34 @@
 
                     <!-- Email Address -->
                     <div>
-                        <x-input-label for="email" :value="('Email')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="email"
-                                      name="email" :value="old('email')" required autofocus autocomplete="username" />
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
                     </div>
 
                     <!-- Password -->
                     <div class="mt-4">
-                        <x-input-label for="password" :value="('Password')" />
-                        <x-text-input id="password" class="block mt-1 w-full" type="password"
-                                      name="password" required autocomplete="current-password" />
+                        <x-input-label for="password" :value="__('Password')" />
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
                         <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
 
                     <!-- Remember Me -->
                     <div class="block mt-4">
                         <label for="remember_me" class="inline-flex items-center">
-                            <input id="remember_me" type="checkbox"
-                                   class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
-                                   name="remember">
-                            <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                            <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                         </label>
                     </div>
 
                     <div class="flex items-center justify-between mt-4">
                         @if (Route::has('password.request'))
-                            <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                               href="{{ route('password.request') }}">
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                                 {{ __('Forgot your password?') }}
                             </a>
                         @endif
 
-                        <x-primary-button class="ms-3">
+                        <x-primary-button class="ml-3">
                             {{ __('Log in') }}
                         </x-primary-button>
                     </div>
@@ -58,5 +52,6 @@
         </div>
     </div>
 </x-guest-layout>
+
 
 
